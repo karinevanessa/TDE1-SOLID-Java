0a4dedca7613d942da814b471bbdd55ca914c0e3
Kah <104913959+karinevanessa@users.noreply.github.com>
2026-09-22 16:12:40 -0300

    feat: adiciona atividade1/Usuario.java

---
+++ atividade1/Usuario.java
@@ -0,0 +1,25 @@
+package atividade1;
+
+public class Usuario {
+    private final String nome;
+    private final String email;
+    private final String senha;
+
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
